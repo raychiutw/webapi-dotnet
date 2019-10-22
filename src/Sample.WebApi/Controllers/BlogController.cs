9310ec910ae8176197ec2a8020b1f87cc916e1b3
raychiutw <lean.lean@gmail.com>
2019-10-22 09:10:00 +0800

    增加 Controller GetRange

--- src/Sample.WebApi/Controllers/BlogController.cs
+++ src/Sample.WebApi/Controllers/BlogController.cs
@@ -78,6 +78,35 @@ namespace Sample.WebApi.Controllers
         }
 
         /// <summary>
+        /// 取得 Blog
+        /// </summary>
+        /// <param name="id">Blog Id</param>
+        /// <returns></returns>
+        [ResponseType(typeof(BlogViewModel))]
+        [HttpGet]
+        public List<BlogViewModel> GetRange(
+            [FromUri]int start,
+            [FromUri]int end)
+        {
+            var blogs = this._blogService.GetAll();
+
+            var models = new List<BlogViewModel>();
+
+            foreach (var blog in blogs)
+            {
+                var model = new BlogViewModel()
+                {
+                    BlogId = blog.BlogId,
+                    Url = blog.Url
+                };
+
+                models.Add(model);
+            }
+
+            return models;
+        }
+
+        /// <summary>
         /// 新增 Blog
         /// </summary>
         /// <param name="parameter">Blog 參數</param>
